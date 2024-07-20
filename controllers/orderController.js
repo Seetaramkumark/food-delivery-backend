856baaf526e4c23d67084256c5ff373603f3653f
seetaramkumark <seetaramkumarkollisetty3342@gmail.com>
2024-07-20 22:40:23 +0530

    seeta

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "https://delight-frontend.netlify.app/"
+    const frontend_url = "https://delight-frontend.netlify.app"
 
     try {
         const newOrder = new orderModel({
